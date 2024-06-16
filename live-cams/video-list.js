1b791effe0abe67115c24117351e88407da18389
Ledesmita <160266656+LedesmitaDJ@users.noreply.github.com>
2024-06-16 17:04:04 -0500

    Add files via upload

---
+++ live-cams/video-list.js
@@ -0,0 +1,57 @@
+let allVideos = [
+   {
+      name: "Times Square Cams - Live",
+      src: "https://www.youtube.com/embed/avCQguy6gN0?autoplay=1",
+      id: "vid_1"
+   },
+   {
+      name: "Times Square 4K View",
+      src: "https://www.youtube.com/embed/QTTTY_ra2Tg?autoplay=1",
+      id: "vid_2"
+   },
+   {
+      name: "Ball Cam 1",
+      src: "Ball-Cam-1.html",
+      id: "vid_3"
+   },
+   {
+      name: "Ball Cam 2",
+      src: "Ball-Cam-2.html",
+      id: "vid_4"
+   },
+   {
+      name: "Duffy Square 4K View",
+      src: "https://www.youtube.com/embed/iiBTWU2FyFo?autoplay=1",
+      id: "vid_5"
+   },
+   {
+      name: "Express View",
+      src: "https://www.youtube.com/embed/a9J1OP_x5Rg?autoplay=1",
+      id: "vid_6"
+   },
+   {
+      name: "1515 Broadway View",
+      src: "https://www.youtube.com/embed/VSSHILnbfyw?autoplay=1",
+      id: "vid_7"
+   },
+   {
+      name: "1540 Broadway View",
+      src: "https://www.youtube.com/embed/h2HXup8nA1I?autoplay=1",
+      id: "vid_8"
+   },
+   {
+      name: "1560 Broadway View",
+      src: "https://www.youtube.com/embed/4qyZLflp-sI?autoplay=1",
+      id: "vid_9"
+   },
+   {
+      name: "Nasdaq",
+      src: "https://livestream.com/accounts/888332/events/931293/player?autoPlay=true&mute=false",
+      id: "vid_10"
+   },
+   {
+      name: "Billboard",
+      src: "https://share.earthcam.net/tJ90CoLmq7TzrY396Yd88A4kdLdbDd6oQl5D9Ktzt8U!/times_square_locations/billboard_view/live",
+      id: "vid_11"
+   }
+]
