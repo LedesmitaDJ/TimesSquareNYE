6d8ad16c922fbc8e3a99559ce88975f18f1539a2
Ledesmita <160266656+LedesmitaDJ@users.noreply.github.com>
2024-06-17 17:40:34 -0500

    Add files via upload

---
+++ nye-live/video-list.js
@@ -0,0 +1,102 @@
+let allVideos = [
+   {
+      name: "NYE 2025 - Live",
+      src: "https://www.youtube.com/embed/NnHYOIDcpLY?autoplay=1",
+      id: "vid_1"
+   },
+   {
+      name: "NYE 2025 - Live Test",
+      src: "https://www.youtube.com/embed/HaMQuJrRmgQ?autoplay=1",
+      id: "vid_2"
+   },
+   {
+      name: "NYE 2025 Live From Times Square",
+      src: "https://www.youtube.com/embed/Fv6CBdzK9pM?autoplay=1",
+      id: "vid_3"
+   },
+   {
+      name: "NYE 2025 Live From Times Square (ASL)",
+      src: "https://www.youtube.com/embed/8v0SCb9gABs?autoplay=1",
+      id: "vid_4"
+   },
+   {
+      name: "NYE 2025 - Live Multi-View",
+      src: "https://www.youtube.com/embed/GZqpy1KQuH8?autoplay=1",
+      id: "vid_5"
+   },
+   {
+      name: "NYE 2025 - Live WebCast",
+      src: "https://livestream.com/accounts/43923/events/10762863/player?autoPlay=true&mute=false",
+      id: "vid_6"
+   },
+   {
+      name: "Live Show",
+      src: "https://player.twitch.tv/?channel=nyewebcast&enableExtensions=true&muted=false&parent=ledesmitadj.github.io&player=popout&volume=1",
+      id: "vid_7"
+   },
+   {
+      name: "Ball Cam",
+      src: "https://player.twitch.tv/?channel=nyeballcam&enableExtensions=true&muted=false&parent=ledesmitadj.github.io&player=popout&volume=1",
+      id: "vid_8"
+   },
+   {
+      name: "Ball Cam 1",
+      src: "Ball-Cam-1.html",
+      id: "vid_9"
+   },
+   {
+      name: "Ball Cam 2",
+      src: "Ball-Cam-2.html",
+      id: "vid_10"
+   },
+   {
+      name: "World Mixed Feed",
+      src: "https://player.twitch.tv/?channel=worldwiderollinglivecam&enableExtensions=true&muted=false&parent=ledesmitadj.github.io&player=popout&volume=1",
+      id: "vid_11"
+   },
+   {
+      name: "Wonderama",
+      src: "https://player.twitch.tv/?channel=wonderamagames&enableExtensions=true&muted=false&parent=ledesmitadj.github.io&player=popout&volume=1",
+      id: "vid_12"
+   },
+   {
+      name: "Times Square Cam",
+      src: "https://player.twitch.tv/?channel=nyetimessquarecam&enableExtensions=true&muted=false&parent=ledesmitadj.github.io&player=popout&volume=1",
+      id: "vid_13"
+   },
+   {
+      name: "Countdown & Downtown Cam",
+      src: "https://player.twitch.tv/?channel=nyecountdowndowntowncam&enableExtensions=true&muted=false&parent=ledesmitadj.github.io&player=popout&volume=1",
+      id: "vid_14"
+   },
+   {
+      name: "New York Mixed Feed",
+      src: "https://player.twitch.tv/?channel=nyemixedfeednyc&enableExtensions=true&muted=false&parent=ledesmitadj.github.io&player=popout&volume=1",
+      id: "vid_15"
+   },
+   {
+      name: "Uptown Cam",
+      src: "https://player.twitch.tv/?channel=nyeuptowncamtsq&enableExtensions=true&muted=false&parent=ledesmitadj.github.io&player=popout&volume=1",
+      id: "vid_16"
+   },
+   {
+      name: "Broadway Plaza",
+      src: "https://player.twitch.tv/?channel=officialnyefeatures&enableExtensions=true&muted=false&parent=ledesmitadj.github.io&player=popout&volume=1",
+      id: "vid_17"
+   },
+   {
+      name: "World Feed",
+      src: "https://player.twitch.tv/?channel=nyeworldfeed&enableExtensions=true&muted=false&parent=ledesmitadj.github.io&player=popout&volume=1",
+      id: "vid_18"
+   },
+   {
+      name: "Audience 1 Cam",
+      src: "https://player.twitch.tv/?channel=nyeaudience1cam&enableExtensions=true&muted=false&parent=ledesmitadj.github.io&player=popout&volume=1",
+      id: "vid_19"
+   },
+   {
+      name: "Audience 2 Cam",
+      src: "https://player.twitch.tv/?channel=nyeaudience2cam&enableExtensions=true&muted=false&parent=ledesmitadj.github.io&player=popout&volume=1",
+      id: "vid_20"
+   }
+]
