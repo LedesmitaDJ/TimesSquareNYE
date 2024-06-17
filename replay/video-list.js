3e98c75ff3fac57bb73f67cb976daf605de67c87
Ledesmita <160266656+LedesmitaDJ@users.noreply.github.com>
2024-06-17 16:40:19 -0500

    Add files via upload

---
+++ replay/video-list.js
@@ -0,0 +1,142 @@
+let allVideos = [
+   {
+      name: "New Year´s Eve 2024",
+      src: "https://livestream.com/accounts/43923/events/10762863/player?autoPlay=true&mute=false",
+      id: "vid_1"
+   },
+   {
+      name: "New Year´s Eve 2023",
+      src: "https://livestream.com/accounts/43923/events/10381259/player?autoPlay=true&mute=false",
+      id: "vid_2"
+   },
+   {
+      name: "New Year´s Eve 2022",
+      src: "https://livestream.com/accounts/43923/events/9872713/player?autoPlay=true&mute=false",
+      id: "vid_3"
+   },
+   {
+      name: "New Year´s Eve 2021",
+      src: "https://livestream.com/accounts/43923/events/9392703/player?autoPlay=true&mute=false",
+      id: "vid_4"
+   },
+   {
+      name: "New Year´s Eve 2020",
+      src: "https://livestream.com/accounts/43923/events/8713964/player?autoPlay=true&mute=false",
+      id: "vid_5"
+   },
+   {
+      name: "New Year´s Eve 2019",
+      src: "https://livestream.com/accounts/43923/events/8452766/player?autoPlay=true&mute=false",
+      id: "vid_6"
+   },
+   {
+      name: "New Year´s Eve 2018",
+      src: "https://livestream.com/accounts/43923/events/7957123/player?autoPlay=true&mute=false",
+      id: "vid_7"
+   },
+   {
+      name: "New Year´s Eve 2017",
+      src: "https://livestream.com/accounts/43923/events/6612528/player?autoPlay=true&mute=false",
+      id: "vid_8"
+   },
+   {
+      name: "New Year´s Eve 2016",
+      src: "https://livestream.com/accounts/43923/events/4458714/player?autoPlay=true&mute=false",
+      id: "vid_9"
+   },
+   {
+      name: "New Year´s Eve 2015",
+      src: "https://www.youtube.com/embed/TeimXHebd_E?autoplay=1",
+      id: "vid_10"
+   },
+   {
+      name: "New Year´s Eve 2014",
+      src: "https://livestream.com/accounts/43923/events/1780083/player?autoPlay=true&mute=false",
+      id: "vid_11"
+   },
+   {
+      name: "New Year´s Eve 2013",
+      src: "https://livestream.com/accounts/43923/events/1673924/player?autoPlay=true&mute=false",
+      id: "vid_12"
+   },
+   {
+      name: "New Year´s Eve 2012",
+      src: "https://livestream.com/accounts/43923/events/389/player?autoPlay=true&mute=false",
+      id: "vid_13"
+   },
+   {
+      name: "New Year´s Eve 2011",
+      src: "https://www.youtube.com/embed/HzEbA2vIuA4?autoplay=1",
+      id: "vid_14"
+   },
+   {
+      name: "New Year´s Eve 2010",
+      src: "https://www.youtube.com/embed/NEZo2Muveqc?autoplay=1",
+      id: "vid_15"
+   },
+   {
+      name: "New Year´s Eve 2009",
+      src: "https://www.youtube.com/embed/PVo-_xJe7aE?autoplay=1",
+      id: "vid_16"
+   },
+   {
+      name: "New Year´s Eve 2008",
+      src: "https://www.youtube.com/embed/gKeHtRk6JCQ?autoplay=1",
+      id: "vid_17"
+   },
+   {
+      name: "New Year´s Eve 2007",
+      src: "https://www.youtube.com/embed/v0E5Zy67P9c?autoplay=1",
+      id: "vid_18"
+   },
+   {
+      name: "New Year´s Eve 2006",
+      src: "https://www.youtube.com/embed/IJmtipkVGJw?autoplay=1",
+      id: "vid_19"
+   },
+   {
+      name: "New Year´s Eve 2005",
+      src: "https://www.youtube.com/embed/n6zU44apAxw?autoplay=1",
+      id: "vid_20"
+   },
+   {
+      name: "New Year´s Eve 2004",
+      src: "https://www.youtube.com/embed/Yf2nUvEELyM?autoplay=1",
+      id: "vid_21"
+   },
+   {
+      name: "New Year´s Eve 2003",
+      src: "https://www.youtube.com/embed/PAtYcvf_uaA?autoplay=1",
+      id: "vid_22"
+   },
+   {
+      name: "New Year´s Eve 2002",
+      src: "https://www.youtube.com/embed/k53UpD6myjA?autoplay=1",
+      id: "vid_23"
+   },
+   {
+      name: "New Year´s Eve 2001",
+      src: "https://www.youtube.com/embed/Xu6DMtbRFf4?autoplay=1",
+      id: "vid_24"
+   },
+   {
+      name: "New Year´s Eve 2000",
+      src: "https://www.youtube.com/embed/65mDUl9nT0o?autoplay=1",
+      id: "vid_25"
+   },
+   {
+      name: "New Year´s Eve 1999",
+      src: "https://www.youtube.com/embed/p2uJLiRaV-s?autoplay=1",
+      id: "vid_26"
+   },
+   {
+      name: "New Year´s Eve 1998",
+      src: "https://www.youtube.com/embed/ryYIYjW8zug?autoplay=1",
+      id: "vid_27"
+   },
+   {
+      name: "New Year´s Eve 1997",
+      src: "https://www.youtube.com/embed/D2dWtzc9S50?autoplay=1",
+      id: "vid_28"
+   }
+]
